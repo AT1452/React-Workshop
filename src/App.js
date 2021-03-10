9ec70c72c568efbf0bec7f9263463075794843b4
Mitchell Bifeld <mbifeld@gmail.com>
2021-03-10 01:18:30 -0600

    Workshop 3

--- src/App.js
+++ src/App.js
@@ -5,13 +5,23 @@ import {
 } from "react-router-dom";
 import Navbar from './components/Navbar';
 import Home from './contents/Home';
+import About from './contents/About';
+import Skills from './contents/Skills';
 
 function App() {
   return (
     <Router>
       <div className = "App">
         <Navbar />
-        <Home />
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/about">
+          <About />
+        </Route>
+        <Route exact path="/skills">
+          <Skills />
+        </Route>
       </div>
     </Router>
 
---
+++ src/contents/About.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+
+class About extends Component {
+    render () {
+        return (
+            <div className = "condiv">
+                <h1>
+                  About Me
+                </h1>
+                <h3>
+                  This is a section about me.
+                </h3>
+            </div>
+        )
+    }
+}
+
+export default About
+
